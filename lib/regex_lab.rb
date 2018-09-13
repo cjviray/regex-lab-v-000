e8eb5bc8e70ced00ed46a905d1894aac64b3c720
Christian V <viray.christianj@gmail.com>
2018-09-13 22:05:58 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,11 @@ text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-text.scan(/\b/w\{5}\b/)
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/[A-Z].+[.!?,]/) != nil (/^[A-Z].+\.$/) 
+text.match(/^[A-Z].+\.$/) 
 end
 
 def valid_phone_number?(phone)
