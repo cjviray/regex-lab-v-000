b8bf55863702b6134a9a9c05ed72a757baf58fab
Christian V <viray.christianj@gmail.com>
2018-09-13 21:54:50 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,11 +3,11 @@ word.match(/\b[aeiou]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/\w+ing/)
+text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-text.scan
+text.scan()
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
